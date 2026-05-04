1751b5186d033441803c04fdccc8fe83889389bf
SvijanyVSC <justine.charlot.univ@gmail.com>
2026-05-04 15:31:03 +0200

    partie01:add exo partie 2 commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,45 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 
+// PARTIE 1
+// function App() {
+//   const DATE = new Date();
+//   return (
+//     <>
+//       <h1>Ma To-Do List Jeux Vidéo</h1>
+//       <h2>{DATE.toLocaleString()}</h2>
+//       <ToDo />
+//     </>
+//   )
+// }
+
+
+// function ToDo() {
+//   return (
+//     <ul>
+//       <li>Baldur's Gate 3</li>
+//       <li>Diablo IV</li>
+//       <li>Enshrouded</li>
+//     </ul>
+//   )
+// }
+
+// PARTIE 2
+function ToDo({todo, date}) {
+return <li>{todo}- {date}</li>
+}
+
 function App() {
-  const DATE = new Date();
   return (
     <>
       <h1>Ma To-Do List Jeux Vidéo</h1>
-      <h2>{DATE.toLocaleString()}</h2>
-      <ToDo />
+      <ul>
+        <ToDo todo={"Baldur's Gate 3"} date={"01/05/2026"}/>
+        <ToDo todo={"Diablo IV"} date={"03/05/2026"}/>
+        <ToDo todo={"Enshrouded"} date={"10/05/2026"}/>
+      </ul>
     </>
-  )
-}
-
-function ToDo() {
-  return (
-    <ul>
-      <li>Baldur's Gate 3</li>
-      <li>Diablo IV</li>
-      <li>Enshrouded</li>
-    </ul>
-  )
+  );
 }
 
 export default App
