e769a3c539f29343aeed4e9c07f8ec75d2018a04
SvijanyVSC <justine.charlot.univ@gmail.com>
2026-05-12 13:55:46 +0200

    partie06: ToDoList complete  partie 6

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import './App.css'
 import { useState } from "react";
 
 const TODOS = [
-  { nom: "Baldur's Gate 3", date: "01-05-2026", heure:10, categorie: "CRPG", checked: true },
-  { nom: "Diablo IV", date: "03-05-2026", heure:16, categorie: "Hack 'n' slash", checked: true },
+  { nom: "Baldur's Gate 3", date: "01-05-2026", heure: 10, categorie: "CRPG", checked: true },
+  { nom: "Diablo IV", date: "03-05-2026", heure: 16, categorie: "Hack 'n' slash", checked: true },
   { nom: "Enshrouded", date: "10-05-2026", heure: 0, categorie: "Survie", checked: false }
 ]
 
@@ -31,7 +31,7 @@ function ToDo({todo, heureRestante}) {
       return <li onClick={() => handleClick(todo.nom)} className='orange'>
         <button onClick={handleClickMinus}> - </button> 
         <button onClick={handleClickPlus}> + </button>
-        {todo.nom} - {todo.date} : {heure}
+        <input type='checkbox'/>{todo.nom} - {todo.date} : {heure}
       </li>
 }
 
@@ -49,6 +49,23 @@ function Form({onSubmit}) {
 
 function App({heureRestante}) {
   const DATE = new Date()
+  const [crpg, setCrpg] = useState( true ) 
+  const [hackSlash, setHackSlash] = useState( true ) 
+  const [survie, setSurvie] = useState( true ) 
+  const TODOLIST = TODOS.filter(element => { 
+         if (element.categorie == "CRPG" && !crpg == true) { 
+      return false 
+    } 
+      if (element.categorie == "Hack 'n' slash" && !hackSlash == true) { 
+      return false 
+    } 
+      if (element.categorie == "Survie" && !survie == true) { 
+      return false 
+    } 
+    return true
+  })
+
+ 
   function handleSubmit(event){
     event.preventDefault();
     const INPUTS = document.querySelectorAll('input[type="text"]');
@@ -60,10 +77,18 @@ function App({heureRestante}) {
       <h1>My Todo List</h1>
       <h2>{DATE.toLocaleString()}</h2>
       <ul>
-        {TODOS.map((todo, index) => (
+        {TODOLIST.map((todo, index) => (
           <ToDo todo={todo} key={index} heureRestante={todo.heure} />
         ))}
       </ul>
+      <section>
+        <label htmlFor="name">CRPG :</label>
+        <input type="checkbox" checked={crpg} onChange = {() => setCrpg( !crpg )} />
+        <label htmlFor="name">Hack 'n' slash :</label>
+        <input type="checkbox" checked={hackSlash} onChange = {() => setHackSlash( !hackSlash)} />
+        <label htmlFor="name" > Survie :</label>
+        <input type="checkbox" checked={survie} onChange = {() => setSurvie( !survie)} />
+      </section>
       <Form onSubmit={event=> handleSubmit(event)} />
     </>
     
@@ -71,4 +96,3 @@ function App({heureRestante}) {
 }
 
 export default App
-
